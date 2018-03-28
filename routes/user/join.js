40b381d1ac1f8ca48cf1aff0cddf4330bf985552
YONG HEE LEE <leeyh2006@nate.com>
2018-03-28 13:43:17 +0900

    join 쿼리 수정

--- routes/user/join.js
+++ routes/user/join.js
@@ -1,26 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var connection  = require('../../src/dbCon.js');
+var pool  = require('../../src/dbCon.js');
 
 router.post('/join.json', function(req, res, next) {
 
     var receiveData = req.body;
-    var sql = 'INSERT INTO USER(USERID,USERPW, USERTEL,USERAD)' +
-        '      VALUES ('+ "'"+receiveData.userId+"' ,"+
-                           "'"+receiveData.userPw+"' ,"+
-                           "'"+receiveData.userTel+"' ,"+
-                           "'"+receiveData.userAd+"'"+')';
-    connection.query(sql,function(err,rows,field){
-        if(!err){
-            res.send(rows);
-            console.log('data insert success');
-        }
-        else{
 
-            console.log(err);
-            res.send("fail");
-        }
+    var user = {
+        "USERID": receiveData.userId,
+        "USERPW": receiveData.userPw,
+        "USERTEL":receiveData.userTel,
+        "USERAD": receiveData.userAd
+    };
+    var sql2 = 'INSERT INTO USER SET ?';
+
+    pool.getConnection(function(err,connection){
+        var query= connection.query(sql2,user,function(err,rows,field){
+            if(err){
+                connection.release();
+                throw err;
+            }
+            console.log(query.sql);
+            res.send(rows);
+            connection.release();
+        });
     });
+
 });
 
 module.exports = router;
--- routes/user/login.js
+++ routes/user/login.js
@@ -1,32 +1,35 @@
 var express = require('express');
 var router = express.Router();
-var connection  = require('../../src/dbCon.js');
+var pool  = require('../../src/dbCon.js');
 
 router.post('/loginCheck.json', function(req, res, next) {
     var receiveData = req.body;
+    var userid = receiveData.userId
+        ,userpw =receiveData.userPw;
 
     var sql =
         "SELECT COUNT(*) AS COUNT" +
         "  FROM USER  " +
-        " WHERE USERID = " +"'"+receiveData.userId+"'"+
-        "   AND USERPW = " +"'"+ receiveData.userPw+"'";
-
-    connection.query(sql,function(err,rows,fields){
-        if(!err){
+        " WHERE USERID = ? " +
+        "  AND USERPW = ? ";
+    pool.getConnection(function(err,connection){
+       var query= connection.query(sql,[userid,userpw],function(err,rows,fields){
+            if(err){
+                connection.release();
+                throw err;
+            }
             if(rows[0].COUNT == 1 ){
+                console.log(query.sql);
                 res.send(rows);
             }
             else{
                 res.send("fail");
             }
+            connection.release();
             console.log("[DEBUG] USER INFO SEARCHED " + rows[0].COUNT);
-        }
-        else
-        {
-            res.send(err);
-        }
-
+        });
     });
+
 
 });
 

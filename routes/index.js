a2532f64025d6ff9c65061b789b0ae9af89f224c
YONG HEE LEE <leeyh2006@nate.com>
2018-03-30 10:12:38 +0900

    카카오톡 콜백 수정

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-router.get('/login', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-router.get('/test/success',function(req,res,next){
-  res.render('index',{title:'exPress'});
-
 });
 module.exports = router;
--- routes/user/login.js
+++ routes/user/login.js
@@ -6,20 +6,49 @@ var KakaoStrategy =require('passport-kakao').Strategy;
 ;
 passport.use('login-kakao',new KakaoStrategy({
         clientID: '8a31ef866bfe1f9c93006c6bfda573f7',
-        callbackURL :'http://localhost:8080/oauth'
+        callbackURL :'http://localhost:8080/login/oauth'
     },
     function (accesToken,refreshToken, profile, done) {
-        console.log(profile);
+        // console.log(profile);
         return done(null,profile);
 
     }
 ));
-/* ***************** */
 
+// 카카오 프로필 세션 저장
+passport.serializeUser(function(user,done){
+    console.log('kakao serialize');
+    // console.log(user);
+    done(null,user);
+
+});
+
+passport.deserializeUser(function(user,done){
+    console.log('kakao desialize');
+    done(null,user);
+});
+/* ***************** */
 
 /* kakao oauth 로그인 */
 router.get('/kakaoLogin',passport.authenticate('login-kakao'));
 
+router.get('/oauth', passport.authenticate('login-kakao', {
+    failureRedirect:'/login/login_fail'
+}),function(req,res){
+    res.redirect('/login/login_success');
+});
+
+router.get('/login_success',function(req,res){
+    console.log(req);
+    res.send(req.user);
+
+});
+
+router.get('/login/login_fail',function (req,res,next) {
+    console.log('kakao Login err');
+    res.render('error', { title: 'Express' });
+
+});
 
 router.post('/loginCheck.json', function(req, res, next) {
     var receiveData = req.body;
@@ -52,4 +81,11 @@ router.post('/loginCheck.json', function(req, res, next) {
 
 });
 
+function ensureAuthenticated(req, res, next) {
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+    if (req.isAuthenticated()) { return next(); }
+    // 로그인이 안되어 있으면, login 페이지로 진행
+    res.redirect('/');
+
+}
 module.exports = router;
